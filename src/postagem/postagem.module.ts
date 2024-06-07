263e236e0996fbeaadbae877af515ea76d482354
gabriel milosz cruz <95766462+Bili20@users.noreply.github.com>
2024-06-06 21:32:52 -0300

    Update postagem.module.ts

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -2,22 +2,40 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostagemEntity } from './models/entites/postagem.entity';
 import { CriaPostUseCase } from './useCases/posta/criaPost.use-case';
-import { PostagenRepo } from './repository/postagenRepo';
+import { PostagenRepo as PostagemRepo } from './repository/postagenRepo';
 import { CriaPostController } from './useCases/posta/criaPost.controller';
-import { SalvarMidiaUsecase } from 'src/midia/usueCases/salvarMidia.use-case';
+import { SalvarMidiaUsecase } from 'src/midia/usueCases/salvarMidia/salvarMidia.use-case';
 import { MidiaRepo } from 'src/midia/repository/midiaRepo';
 import { MidiaEntity } from 'src/midia/models/entities/midia.entity';
+import { PegarPostesPessoaUseCase } from './useCases/pegaPostesPessoa/pegaPostesPessoa.use-case';
+import { PegarPostesPessoaController } from './useCases/pegaPostesPessoa/pegaPostesPessoa.controller';
+import { DeletaMidiaUseCase } from 'src/midia/usueCases/deletaMidia/deletaMidia.use-case';
+import { DeletaPostePessoaUseCase } from './useCases/deletaPostePessoa/deletaPostePessoa.use-case';
+import { DeletaPostPessoaController } from './useCases/deletaPostePessoa/deletaPostePessoa.controller';
+import { PegaUmPostePessoaUseCase } from './useCases/pegaUmpostePessoa/pegaUmPostePessoa.use-case';
+import { PegaPostesUseCase } from './useCases/pegaPostes/pegaPostes.use-case';
+import { PegaPostesController } from './useCases/pegaPostes/pegaPostes.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PostagemEntity, MidiaEntity])],
   providers: [
     CriaPostUseCase,
-    PostagenRepo,
-    { provide: 'IPostagenRepo', useExisting: PostagenRepo },
+    PegarPostesPessoaUseCase,
+    DeletaPostePessoaUseCase,
+    PegaUmPostePessoaUseCase,
+    PegaPostesUseCase,
+    PostagemRepo,
+    { provide: 'IPostagenRepo', useExisting: PostagemRepo },
     SalvarMidiaUsecase,
+    DeletaMidiaUseCase,
     MidiaRepo,
     { provide: 'IMidiaRepo', useExisting: MidiaRepo },
   ],
-  controllers: [CriaPostController],
+  controllers: [
+    CriaPostController,
+    PegarPostesPessoaController,
+    DeletaPostPessoaController,
+    PegaPostesController,
+  ],
 })
 export class PostagemModule {}
