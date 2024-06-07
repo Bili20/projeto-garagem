7bed5cc202b89103ee35b0a3e79f9d6b0093e761
gabriel milosz cruz <95766462+Bili20@users.noreply.github.com>
2024-06-06 21:33:07 -0300

    Create pegaUmPostePessoa.use-case.ts

---
+++ src/postagem/useCases/pegaUmpostePessoa/pegaUmPostePessoa.use-case.ts
@@ -0,0 +1,28 @@
+import {
+  BadRequestException,
+  HttpException,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { IPostagenRepo } from 'src/postagem/models/interfaces/postagenRepo.interface';
+
+@Injectable()
+export class PegaUmPostePessoaUseCase {
+  @Inject('IPostagenRepo')
+  private readonly postagenRepo: IPostagenRepo;
+
+  async execute(id: number) {
+    try {
+      const post = await this.postagenRepo.umPoste(id);
+      if (!post) {
+        throw new BadRequestException({ message: 'Postagem não encontrada' });
+      }
+      return post;
+    } catch (e) {
+      throw new HttpException(
+        e.response ?? 'Erro ao tenatr procurar post',
+        e.status ?? 400,
+      );
+    }
+  }
+}
