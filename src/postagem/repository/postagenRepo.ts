ec31cdfd1b53617a30cbb3d0ceb6778a2ba50c24
gabriel milosz cruz <95766462+Bili20@users.noreply.github.com>
2024-06-06 21:32:54 -0300

    Update postagenRepo.ts

--- src/postagem/repository/postagenRepo.ts
+++ src/postagem/repository/postagenRepo.ts
@@ -4,14 +4,39 @@ import { CriarPostagemDTO } from '../models/dtos/criarPostagen.dto';
 import { PostagemEntity } from '../models/entites/postagem.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { PegaPostesDTO } from '../models/dtos/pegaPostes.dto';
 
 @Injectable()
 export class PostagenRepo implements IPostagenRepo {
   constructor(
     @InjectRepository(PostagemEntity)
-    private readonly postagenRepo: Repository<PostagemEntity>,
+    private readonly postagemRepo: Repository<PostagemEntity>,
   ) {}
   async criar(param: CriarPostagemDTO): Promise<PostagemEntity> {
-    return await this.postagenRepo.save(param);
+    return await this.postagemRepo.save(param);
+  }
+
+  async postesPessoa(idPessoa: number): Promise<PostagemEntity[]> {
+    return await this.postagemRepo.find({
+      where: { idPessoa: idPessoa },
+      relations: { midia: true },
+    });
+  }
+
+  async postes(params: PegaPostesDTO): Promise<PostagemEntity[]> {
+    return this.postagemRepo
+      .createQueryBuilder('post')
+      .orderBy('post.dataCadastro', 'DESC')
+      .take(params.quantidade)
+      .skip((params.pagina - 1) * params.quantidade)
+      .getMany();
+  }
+
+  async umPoste(id: number): Promise<PostagemEntity> {
+    return this.postagemRepo.findOne({ where: { id: id } });
+  }
+
+  async deletaPost(param: PostagemEntity): Promise<void> {
+    await this.postagemRepo.delete(param.id);
   }
 }
