0c011433a2acf698535b1ee6b0ba09b1a8437b9c
gabriel milosz cruz <95766462+Bili20@users.noreply.github.com>
2024-06-06 21:32:31 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use('/files', express.static(path.join(__dirname, '..', 'files')));
+  app.enableCors();
   await app.listen(3003);
 }
 bootstrap();
