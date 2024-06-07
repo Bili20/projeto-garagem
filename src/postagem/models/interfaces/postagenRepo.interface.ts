b83f38ed6ca8a92b7b0d0a3a1c67831f4916fc40
gabriel milosz cruz <95766462+Bili20@users.noreply.github.com>
2024-06-06 21:32:51 -0300

    Update postagenRepo.interface.ts

--- src/postagem/models/interfaces/postagenRepo.interface.ts
+++ src/postagem/models/interfaces/postagenRepo.interface.ts
@@ -1,6 +1,11 @@
 import { CriarPostagemDTO } from '../dtos/criarPostagen.dto';
+import { PegaPostesDTO } from '../dtos/pegaPostes.dto';
 import { PostagemEntity } from '../entites/postagem.entity';
 
 export interface IPostagenRepo {
   criar(param: CriarPostagemDTO): Promise<PostagemEntity>;
+  postesPessoa(idPessoa: number): Promise<PostagemEntity[]>;
+  postes(params: PegaPostesDTO): Promise<PostagemEntity[]>;
+  umPoste(id: number): Promise<PostagemEntity>;
+  deletaPost(param: PostagemEntity): Promise<void>;
 }
