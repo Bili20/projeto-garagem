3e3764e9efb8f7bdc552508b3b5239ee7b16acd5
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-06-11 17:53:17 -0300

    Update autenticacao.module.ts

--- src/autenticacao/autenticacao.module.ts
+++ src/autenticacao/autenticacao.module.ts
@@ -3,9 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoaEntity } from 'src/pessoa/models/entities/pessoa.entity';
 import { ValidaUsuarioUseCase } from './useCases/validaUsuario/validaUsuario.use-case';
 import { AutenticacaoRepo } from './repository/autenticacaoRepo';
-
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [TypeOrmModule.forFeature([PessoaEntity])],
+  imports: [
+    TypeOrmModule.forFeature([PessoaEntity]),
+    PassportModule,
+    JwtModule.register({
+      secret: 'asduhad89ayd98adyh0ahda0d',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [
     ValidaUsuarioUseCase,
     AutenticacaoRepo,
