e036f69c95a7d1f63b13b58a4cc1c18a93f51f39
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-06-11 10:35:10 -0300

    Update midia.module.ts

--- src/midia/midia.module.ts
+++ src/midia/midia.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MidiaEntity } from './models/entities/midia.entity';
-import { SalvarMidiaUsecase } from './usueCases/salvarMidia/salvarMidia.use-case';
+import { SalvarMidiaPostUsecase } from './usueCases/salvarMidiaPost/salvarMidia.use-case';
 import { MidiaRepo } from './repository/midiaRepo';
 import { DeletaMidiaUseCase } from './usueCases/deletaMidia/deletaMidia.use-case';
+import { SalvarMidiaPerfilUseCase } from './usueCases/salvarMidiaPerfil/salvarMidiaPerfil.use-case';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MidiaEntity])],
   providers: [
-    SalvarMidiaUsecase,
+    SalvarMidiaPostUsecase,
+    SalvarMidiaPerfilUseCase,
     DeletaMidiaUseCase,
     MidiaRepo,
     { provide: 'IMidiaRepo', useExisting: MidiaRepo },
