f0d2e9489b68d6bcb0609ca3dbe913723ffeb785
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-06-11 11:32:53 -0300

    Update buscaPessoas.use-case.ts

--- src/pessoa/useCases/buscaPessoas/buscaPessoas.use-case.ts
+++ src/pessoa/useCases/buscaPessoas/buscaPessoas.use-case.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { URLIMAGE } from 'src/constants/constants';
 import { IPessoaRepo } from 'src/pessoa/models/interfaces/pessoa.interface';
 
 @Injectable()
@@ -7,6 +8,12 @@ export class BuscaPessoasUsecase {
   private readonly pessoaRepo: IPessoaRepo;
 
   async execute() {
-    return await this.pessoaRepo.buscarPessoas();
+    const pessoas = await this.pessoaRepo.buscarPessoas();
+    for (const pessoa of pessoas) {
+      if (pessoa.midia) {
+        pessoa.midia.nome = URLIMAGE + '/files/perfil/' + pessoa.midia.nome;
+      }
+    }
+    return pessoas;
   }
 }
