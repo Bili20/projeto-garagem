57c5edcac72d8c0cab1e4f4adb5f6b639b82c451
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-06-11 10:35:39 -0300

    Create ataulizaCadastro.controller.ts

---
+++ src/pessoa/useCases/atualizaCadastro/ataulizaCadastro.controller.ts
@@ -0,0 +1,65 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Inject,
+  Param,
+  Patch,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { AtualizaMidiaUseCase } from 'src/midia/usueCases/ataulizaMidia/atualizaMidia.use-case';
+import { AtualizaPessoaDTO } from 'src/pessoa/models/dtos/atualizaPessoa.dto';
+import { AtualizaCadastroUseCase } from './atualizaCadastro.use-case';
+import { existsSync, unlinkSync } from 'fs';
+
+@Controller('atauliza/informacoes')
+export class AtualizaCadastroController {
+  @Inject(AtualizaCadastroUseCase)
+  private readonly atualizaCadastroUseCase: AtualizaCadastroUseCase;
+  @Inject(AtualizaMidiaUseCase)
+  private readonly atualizaMidiaUseCase: AtualizaMidiaUseCase;
+
+  @UseInterceptors(
+    FileInterceptor('file', {
+      limits: { fileSize: 10 * 1024 * 1024 },
+      storage: diskStorage({
+        destination: './files/perfil',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  @Patch(':id')
+  async atualizaCadastro(
+    @Param('id') id: number,
+    @Body() param: AtualizaPessoaDTO,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
+    try {
+      const data = await this.atualizaCadastroUseCase.execute(id, param);
+      const idMidia = data.midia?.id;
+      if (file) {
+        await this.atualizaMidiaUseCase.execute(idMidia, {
+          nome: file.filename,
+          idPessoa: id,
+        });
+      }
+    } catch (e) {
+      if (file) {
+        if (existsSync('files/perfil/' + file.filename)) {
+          unlinkSync('files/perfil/' + file.filename);
+        }
+      }
+      throw new BadRequestException({ message: 'Erro ao atualizar dados' });
+    }
+  }
+}
