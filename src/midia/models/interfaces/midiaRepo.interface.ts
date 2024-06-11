527df4a981d17e3a022b6521ed91023b1a2d7e4b
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-06-11 10:35:21 -0300

    Update midiaRepo.interface.ts

--- src/midia/models/interfaces/midiaRepo.interface.ts
+++ src/midia/models/interfaces/midiaRepo.interface.ts
@@ -1,8 +1,9 @@
-import { SalvarMidiaDTO } from '../dtos/salvarMidia.dto';
 import { MidiaEntity } from '../entities/midia.entity';
 
 export interface IMidiaRepo {
-  salvar(param: SalvarMidiaDTO): Promise<void>;
+  salvar(param: MidiaEntity): Promise<void>;
   deletar(param: MidiaEntity): Promise<void>;
   buscaMidias(idPostagem: number): Promise<MidiaEntity[]>;
+  buscaUm(id: number): Promise<MidiaEntity>;
+  atualizar(id: number, param: MidiaEntity): Promise<void>;
 }
