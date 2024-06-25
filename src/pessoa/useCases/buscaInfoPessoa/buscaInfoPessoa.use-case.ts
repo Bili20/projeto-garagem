f485d06196d40d59ad2a6172b325d35a4d31e268
gabriel milosz cruz <95766462+Bili20@users.noreply.github.com>
2024-06-24 22:00:13 -0300

    Update buscaInfoPessoa.use-case.ts

--- src/pessoa/useCases/buscaInfoPessoa/buscaInfoPessoa.use-case.ts
+++ src/pessoa/useCases/buscaInfoPessoa/buscaInfoPessoa.use-case.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { IPessoaRepo } from 'src/pessoa/models/interfaces/pessoa.interface';
+import { URLIMAGE } from 'src/utils/constants/constants';
 import { UsuarioAtualUseCase } from 'src/utils/usuarioAtual/usuarioAtual.use-case';
 
 @Injectable()
@@ -13,6 +14,9 @@ export class BuscaInfoPessoaUseCase {
   async execute(req: Request) {
     const idPessoa = (await this.usuarioAtualUseCase.execute(req)).id;
     const pessoa = await this.pessoaRepo.buscaInformacoePessoa(idPessoa);
+    if (pessoa.midia) {
+      pessoa.midia.nome = URLIMAGE + '/files/perfil/' + pessoa.midia.nome;
+    }
     delete pessoa.senha;
     return pessoa;
   }
